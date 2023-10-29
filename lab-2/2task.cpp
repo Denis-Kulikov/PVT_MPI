a13be0e45aa750ce819a01dafa863c9b22b6ea1b
Denis Kulikov <denis_kulikov_a_2003@mail.ru>
2023-10-29 21:01:03 +0700

    Task 1 ready

--- lab-2/2task.cpp
+++ lab-2/2task.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <mpi.h>
+
+const float eps = 1E-6; 
+const int n0 = 10000;
+
+const float a = 0.1f;
+const float b = 1.0f;
+
+float func (float x)
+{
+    return log(1 + x) / x;
+}
 
 int main(int argc, char **argv)
 {
@@ -13,12 +25,10 @@ int main(int argc, char **argv)
 
     for (k = 0; delta > eps; n *= 2, k ^= 1) {
         int points_per_proc = n / commsize;
-        int lb = rank * points_per_proc;
-        int ub = (rank == commsize - 1) ? (n - 1) : (lb + points_per_proc - 1);
         double h = (b - a) / n;
         double s = 0.0;
 
-        for (int i = lb; i <= ub; i++)
+        for (int i = rank; i <= n; i += commsize)
             s += func(a + h * (i + 0.5));
         MPI_Allreduce(&s, &sq[k], 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
         sq[k] *= h;
@@ -30,5 +40,6 @@ int main(int argc, char **argv)
         printf("Result Pi: %.12f; Runge rule: EPS %e, n %d\n", sq[k] * sq[k], eps, n / 2);
 
     MPI_Finalize();
+    
     return 0;
 }
